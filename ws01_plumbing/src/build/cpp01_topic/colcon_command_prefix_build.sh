b6bebef61be53e649dc119ed6b11cc4bd9bbc19a
zhangyiming886 <1294120660@qq.com>
2024-11-04 20:45:19 +0800

    master

--- ws01_plumbing/src/build/cpp01_topic/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/amon/桌面/Ros2_learning/ws01_plumbing/src/install/base_interfaces_demo/share/base_interfaces_demo/package.sh"
